ce090993633a49e615f7e3363b3c9fc0fa0332b0
Guilherme Sequeira <gsequeira@adminisrsimac14.americas.media.global.loc>
2016-08-08 17:51:23 -0300

    ajuste database

--- test/data.js
+++ test/data.js
@@ -5,16 +5,16 @@ var contatos = [
 	{nome: "xyz3", email: "xyz3@email.com.br"}
 ];
 
-MongoClient.connect('mongodb://127.0.0.1L27017/contatooh_test'), function(erro, db){
+MongoClient.connect('mongodb://127.0.0.1:27017/contatooh_test', function(erro, db){
 	if(erro) throw err;
 
-	db.drobDatabase(function(err){
+	db.dropDatabase(function(err){
 		if(err) return console.log(err);
 		console.log('Banco apagado com sucesso');
 		db.collection('contatos').insert(contatos, function(err, inserted){
 			if(err) return console.log(err);
 			console.log('Banco populado com sucesso');
-			process.exit(0);
+			process.exit(0)
 		})
 	})
-};
+});
