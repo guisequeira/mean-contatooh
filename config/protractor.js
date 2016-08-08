ffb750033b91e036c4602d596a434e78d13b85af
Guilherme Sequeira <gsequeira@adminisrsimac14.americas.media.global.loc>
2016-08-08 18:53:51 -0300

    ajuste database

--- config/protractor.js
+++ config/protractor.js
@@ -3,12 +3,35 @@ var config = require('../app/config/config')();
 exports.config = {
 	sauceUser: config.sauceUser,
 	sauceKey: config.sauceKey,
-	capabilities: {
-		'name': config.sauceTestName,
-		'browserName': 'chrome',
-		'tunnel-identifier': config.travisJobNumber,
-		'build': config.travisBuild
-	},
+	// capabilities: {
+	// 	'name': config.sauceTestName,
+	// 	'browserName': 'chrome',
+	// 	'tunnel-identifier': config.travisJobNumber,
+	// 	'build': config.travisBuild
+	// },
+	multiCapabilities:[
+		{
+			platformName: 'iOS',
+			platformVersion: '7.1',
+			browserName: '',
+			app: 'safari',
+			deviceName: 'iPhone Simulator', 
+			'appium-version': '1.4.0',
+		},
+		{
+			platformName: 'Android',
+			platformVersion: '4.4',
+			browserName: 'Browser',
+			deviceName: 'Android Emulator', 
+			'appium-version': '1.4.0',
+		},
+		{
+			'name': config.sauceTestName,
+			'browserName': 'chrome',
+			'tunnel-identifier': config.travisJobNumber,
+			'build': config.travisBuild
+		}
+	],
 	
 	specs: ['../test/e2e/**/*.js'],
 	onPrepare: function(){
