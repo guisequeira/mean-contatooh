1307f81cccc7bc104ffb8a53b497ab028098dd3c
Guilherme Sequeira <gsequeira@adminisrsimac14.americas.media.global.loc>
2016-08-09 11:04:17 -0300

    multicapabilities add

--- config/protractor.js
+++ config/protractor.js
@@ -3,15 +3,53 @@ var config = require('../app/config/config')();
 exports.config = {
 	sauceUser: config.sauceUser,
 	sauceKey: config.sauceKey,
-	capabilities: {
-		browserName: 'safari',
-    'appium-version': '1.0',
-    platformName: 'iOS',
-    platformVersion: '7.1',
-    deviceName: 'iPhone Simulator',
-    'tunnel-identifier': config.travisJobNumber,
-		'build': config.travisBuild
-	},
+	multiCapabilities: [
+	{
+    // by default, these first two browsers will come up in 
+    // Linux if you don't specify an OS
+    'name': 'Chrome',
+    'browserName': 'chrome'
+  }, 
+  {
+    'name': 'Firefox',
+    'browserName': 'firefox'
+  }, 
+  {
+    'name': 'Win XP/IE8',
+    'os': 'Windows XP',
+    'browserName': 'internet explorer',
+    'version': '8.0'
+  }, 
+  {
+    'name': 'Win7/IE8',
+    'os': 'Windows 7',
+    'browserName': 'internet explorer',
+    'version': '8.0'
+  }, 
+  {
+    'name': 'Win7/IE9',
+    'os': 'Windows 7',
+    'browserName': 'internet explorer',
+    'version': '9.0'
+  }, 
+  {
+    'name': 'Win8/IE10',
+    'os': 'Windows 8',
+    'browserName': 'internet explorer',
+    'version': '10.0'
+  }, 
+  {
+    'name': 'Win8.1/IE11',
+    'os': 'Windows 8.1',
+    'browserName': 'internet explorer',
+    'version': '11.0'
+  }],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+      showColors: true,
+      defaultTimeoutInterval: 30000
+  },
 	
 	specs: ['../test/e2e/**/*.js'],
 	onPrepare: function(){
